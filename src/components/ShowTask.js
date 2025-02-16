e89094886840dd25c36388da58a2d62b76b6c954
Sasi <sasikiran.tt3011@gmail.com>
2025-02-16 15:48:05 +0530

    added task.map

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,4 +1,10 @@
 export const ShowTask = () => {
+  const task = [{id: 10001, name: "Task A", time: "2:09:01 AM 9/14/2030"},
+    {id: 10002, name: "Task B", time: "2:09:01 AM 9/14/2030"},
+    {id: 10003, name: "Task C", time: "2:09:01 AM 9/14/2030"},
+  ];
+
+
   return (
     <section className="showTask">
         <div className="head">
@@ -9,14 +15,17 @@ export const ShowTask = () => {
             <button className="clearAll">Clear All</button>
         </div>
         <ul>
-          <li>
-            <p>
-              <span className="name">Task A</span>
-              <span className="time">2:09:01 AM 9/14/2030</span>
-            </p>
-            <i class="bi bi-pencil-square"></i>
-            <i class="bi bi-trash"></i>
-          </li>
+            {task.map((task) => (
+                <li>
+                  <p>
+                    <span className="name">{task.name}</span>
+                    <span className="time">{task.time}</span>
+                </p>
+                <i class="bi bi-pencil-square"></i>
+                <i class="bi bi-trash"></i>
+              </li>
+            ))}
+          
         </ul>
     </section>
   )
