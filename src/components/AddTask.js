4c8cdaaaeaecad8622b81e81ceb37677e4f70deb
Sasi <sasikiran.tt3011@gmail.com>
2025-02-16 18:17:41 +0530

    adding addtask changes

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,17 @@
-export const AddTask = () => {
+export const AddTask = (tasklist, setTasklist) => {
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const date=new Date();
+      const newTask={
+          id: date.getTime(),
+          name: e.target.task.value,
+          time:`${date.toLocaleTimeString()}:${date.toLocaleDateString()}`
+      }
+      setTasklist([...tasklist,newTask]);
+  }
   return (
     <section className="addTask">
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Add Task" name="task" autoComplete="off" maxLength="25"/>
             <button type="submit">Add</button>
         </form>
--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,32 +1,25 @@
-export const ShowTask = () => {
-  const task = [{id: 10001, name: "Task A", time: "2:09:01 AM 9/14/2030"},
-    {id: 10002, name: "Task B", time: "2:09:01 AM 9/14/2030"},
-    {id: 10003, name: "Task C", time: "2:09:01 AM 9/14/2030"},
-  ];
-
-
+export const ShowTask = ({tasklist, setTasklist}) => {
   return (
-    <section className="showTask">
-        <div className="head">
-            <div>
-              <span className="title">Todo</span>
-              <span className="count">0</span>  
-            </div>
-            <button className="clearAll">Clear All</button>
-        </div>
-        <ul>
-            {task.map((task) => (
-                <li>
-                  <p>
-                    <span className="name">{task.name}</span>
-                    <span className="time">{task.time}</span>
-                </p>
-                <i class="bi bi-pencil-square"></i>
-                <i class="bi bi-trash"></i>
-              </li>
-            ))}
-          
-        </ul>
-    </section>
-  )
+      <section className='showTask'>
+          <div className="head">
+              <div>
+                  <span className="title">Todo</span>
+                  <span className="count">{tasklist.length}</span>
+              </div>
+              <button className="clearAll">Clear All</button>
+          </div>
+          <ul>
+              {tasklist.map((task) => (
+                  <li>
+                      <p>
+                          <span className="name">{task.name}</span>
+                          <span className="time">{task.time}</span>
+                      </p>                
+                      <i className="bi bi-pencil-square"></i>
+                      <i className="bi bi-trash"></i>
+                  </li>
+              ))}
+          </ul>
+      </section>
+  );
 }
