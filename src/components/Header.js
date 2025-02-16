ff54aaa4a68298e2258047781da42427dae113fd
Sasi <sasikiran.tt3011@gmail.com>
2025-02-16 12:14:23 +0530

    added theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,14 @@ export const Header = () => {
             <img src={Logo} alt="Taskmate Logo" />
             <span>TaskMate</span>
         </div>
+        <div className="themeSelector">
+            <span className="light"></span>
+            <span className="medium"></span>
+            <span className="dark"></span>
+            <span className="gOne"></span>
+            <span className="gTwo"></span>
+            <span className="gThree"></span>
+        </div>
     </header>
   )
 }
